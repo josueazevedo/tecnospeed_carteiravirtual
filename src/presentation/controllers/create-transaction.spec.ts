c483a9346df614393731c11bd1eb2d6e20c61b32
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-26 22:18:07 -0300

    test: ensure CreateTransactionController returns 400 if Missing Param

---
+++ src/presentation/controllers/create-transaction.spec.ts
@@ -0,0 +1,39 @@
+import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
+import { Validation } from '../protocols/validation'
+import { CreateTransactionController } from './create-transaction'
+
+const makeSut = (): any => {
+  const fakeRequest = {
+    body: {
+      value: 'any_value',
+      operation: 'any_operation'
+    }
+  }
+  class ValidationSpy implements Validation {
+    error: Error = null
+    input: any
+
+    validate (input: string): Error {
+      this.input = input
+      return this.error
+    }
+  }
+  const validationSpy = new ValidationSpy()
+  const sut = new CreateTransactionController(validationSpy)
+  return {
+    sut,
+    validationSpy,
+    fakeRequest
+  }
+}
+
+describe('Create Transaction Controller', () => {
+  test('Should return 400 if Validation return Missing Param', async () => {
+    const { sut, validationSpy, fakeRequest } = makeSut()
+    jest.spyOn(validationSpy, 'validate').mockReturnValueOnce(new MissingParamError('any_param'))
+    const httpResponse = await sut.handle(fakeRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_param')))
+  })
+})
