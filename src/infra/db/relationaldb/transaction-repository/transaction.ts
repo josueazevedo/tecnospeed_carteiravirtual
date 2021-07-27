f4704d22943933a402dec543fe9d4814dfeb0ec2
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-27 12:41:05 -0300

    refactor: ensure TransactionRelacionalRepository implements GetBalanceRepository protocols

--- src/infra/db/relationaldb/transaction-repository/transaction.ts
+++ src/infra/db/relationaldb/transaction-repository/transaction.ts
@@ -1,11 +1,24 @@
+import { Sequelize } from 'sequelize'
 import { addTransactionRepository } from '../../../../data/protocols/add-transaction-repository'
+import { GetBalanceRepository } from '../../../../data/protocols/get-balance-repository'
+import { BalanceModel } from '../../../../domain/models/balance'
 import { TransactionModel } from '../../../../domain/models/transaction'
 import { AddTransactionModel } from '../../../../domain/usecases/add-transaction'
 import { Transaction } from '../models/transaction'
 
-export class TransactionRelacionalRepository implements addTransactionRepository {
+export class TransactionRelacionalRepository implements addTransactionRepository, GetBalanceRepository {
   async add (transaction: AddTransactionModel): Promise<TransactionModel> {
     const newTransaction = await Transaction.create(transaction, { raw: true })
     return newTransaction
+  }
+
+  async getBalance (): Promise<BalanceModel> {
+    const balance = await Transaction.findOne({
+      attributes: [
+        [Sequelize.literal("SUM((CASE operation WHEN 'out' THEN (value * -1) ELSE value END))"), 'value']
+      ],
+      raw: true
+    })
+    return balance
   }
 }
