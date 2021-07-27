b8dca59a5b1ca2dcc6ee082feac5850ede0393fd
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-27 18:58:18 -0300

    test: ensure CreateCategoryController returns 500 if AddCategory throws

--- src/presentation/controllers/create-category.spec.ts
+++ src/presentation/controllers/create-category.spec.ts
@@ -1,4 +1,7 @@
+import { CategoryModel } from '../../domain/models/category'
+import { AddCategory, AddCategoryModel } from '../../domain/usecases/add-category'
 import { MissingParamError } from '../errors/missing-param-error'
+import { ServerError } from '../errors/server-error'
 import { badRequest } from '../helpers/http-helper'
 import { Validation } from '../protocols/validation'
 import { CreateCategoryController } from './create-category'
@@ -9,7 +12,15 @@ const makeSut = (): any => {
       name: 'any_name'
     }
   }
-
+  class AddCategoryStub implements AddCategory {
+    async add (transaction: AddCategoryModel): Promise<CategoryModel> {
+      const fakeCategory = {
+        id: 1,
+        name: 'any_name'
+      }
+      return new Promise(resolve => resolve(fakeCategory))
+    }
+  }
   class ValidationSpy implements Validation {
     error: Error = null
     input: any
@@ -19,10 +30,12 @@ const makeSut = (): any => {
     }
   }
   const validationSpy = new ValidationSpy()
-  const sut = new CreateCategoryController(validationSpy)
+  const addCategoryStub = new AddCategoryStub()
+  const sut = new CreateCategoryController(validationSpy, addCategoryStub)
   return {
     sut,
     validationSpy,
+    addCategoryStub,
     fakeRequest
   }
 }
@@ -34,5 +47,15 @@ describe('Create Transaction Controller', () => {
     const httpResponse = await sut.handle(fakeRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('any_param')))
+  })
+
+  test('Should return 500 if AddCategory return throws', async () => {
+    const { sut, addCategoryStub, fakeRequest } = makeSut()
+    jest.spyOn(addCategoryStub, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(fakeRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
