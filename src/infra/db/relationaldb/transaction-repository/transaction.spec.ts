d3f6c3398d197fbd07acc477b60f99f9defb5dc6
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-27 12:41:18 -0300

    test: ensure TransactionRelacionalRepository implements GetBalanceRepository protocols

--- src/infra/db/relationaldb/transaction-repository/transaction.spec.ts
+++ src/infra/db/relationaldb/transaction-repository/transaction.spec.ts
@@ -6,6 +6,10 @@ describe('Transaction RelationalDB', () => {
     await Transaction.destroy({ where: {} })
   })
 
+  beforeEach(async () => {
+    await Transaction.destroy({ where: {} })
+  })
+
   test('Should return an transaction on success', async () => {
     const sut = new TransactionRelacionalRepository()
     const transaction = await sut.add({
@@ -17,6 +21,22 @@ describe('Transaction RelationalDB', () => {
     expect(transaction.id).toBeTruthy()
     expect(transaction.value).toBe('1')
     expect(transaction.operation).toBe('in')
-    expect(transaction.notes).toBe('any_notes')
+  })
+
+  test('Should return an balance on success', async () => {
+    const sut = new TransactionRelacionalRepository()
+    await sut.add({
+      value: 20,
+      operation: 'in',
+      notes: 'any_notes'
+    })
+    await sut.add({
+      value: 5,
+      operation: 'out',
+      notes: 'any_notes'
+    })
+    const balance = await sut.getBalance()
+    expect(balance).toBeTruthy()
+    expect(balance.value).toBe('15')
   })
 })
