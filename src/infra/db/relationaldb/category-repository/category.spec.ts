535ff0b0c8d103d8cfb80bd166665f9496538493
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-27 21:50:41 -0300

    test: ensure CategoryRelacionalRepository if it doesnt find return null

--- src/infra/db/relationaldb/category-repository/category.spec.ts
+++ src/infra/db/relationaldb/category-repository/category.spec.ts
@@ -6,6 +6,10 @@ describe('Category RelationalDB', () => {
     await Category.destroy({ where: {} })
   })
 
+  beforeEach(async () => {
+    await Category.destroy({ where: {} })
+  })
+
   test('Should return an category on success', async () => {
     const sut = new CategoryRelacionalRepository()
     const category = await sut.add({
@@ -14,5 +18,27 @@ describe('Category RelationalDB', () => {
     expect(category).toBeTruthy()
     expect(category.id).toBeTruthy()
     expect(category.name).toBe('any_name')
+  })
+
+  test('Should return a null if it doesnt find', async () => {
+    const sut = new CategoryRelacionalRepository()
+    const upUategory = await sut.update({
+      id: 99,
+      name: 'new_name'
+    })
+    expect(upUategory).toBeNull()
+  })
+
+  test('Should return an category on success', async () => {
+    const sut = new CategoryRelacionalRepository()
+    const category = await sut.add({
+      name: 'any_name'
+    })
+    const upUategory = await sut.update({
+      id: category.id,
+      name: 'new_name'
+    })
+    expect(category).toBeTruthy()
+    expect(upUategory.name).toBe('new_name')
   })
 })
