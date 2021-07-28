a0bff215a5a1d7292eec465eb6c59ee160ddeb25
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-28 20:48:03 -0300

    feat: create router for GetTransactionPeriodController

--- src/main/routes/transaction-routes.ts
+++ src/main/routes/transaction-routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapter/express-route-adapter'
 import { makeCreateTransactionController } from '../factories/transactions/create-transaction-controller-factory'
 import { makeGetBalanceController } from '../factories/transactions/get-balance-controller-factory'
+import { makeGetTransactionPeriodController } from '../factories/transactions/get-transactions-period-controller-factory'
 
 export default (router: Router): void => {
   router.post('/transaction', adaptRoute(makeCreateTransactionController()))
   router.get('/balance', adaptRoute(makeGetBalanceController()))
+  router.get('/transaction', adaptRoute(makeGetTransactionPeriodController()))
 }
