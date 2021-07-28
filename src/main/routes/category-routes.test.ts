dd1bc76aec4cecc4217741f449ac6263e78ebd69
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-27 22:36:38 -0300

    test: add Update Category Router

--- src/main/routes/category-routes.test.ts
+++ src/main/routes/category-routes.test.ts
@@ -9,4 +9,26 @@ describe('Category Router', () => {
       })
       .expect(200)
   })
+
+  test('Should return 404 if not find category', async () => {
+    await request(app).put('/category ')
+      .send({
+        id: 1,
+        name: 'any_name'
+      })
+      .expect(404)
+  })
+
+  test('Should return an category on success', async () => {
+    await request(app).post('/category ')
+      .send({
+        name: 'any_name'
+      }).then(async category => {
+        await request(app).put('/category')
+          .send({
+            id: category.body.id,
+            name: 'new_name'
+          }).expect(200)
+      })
+  })
 })
