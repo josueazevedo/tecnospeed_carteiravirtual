4ec7dc7b2504c1cf41e260be417f19342d6173d5
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-27 22:36:16 -0300

    feat: add Update Category Router

--- src/main/routes/category-routes.ts
+++ src/main/routes/category-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapter/express-route-adapter'
-import { makeCreateCategoryController } from '../factories/create-category-controller-factory'
+import { makeCreateCategoryController } from '../factories/categories/create-category-controller-factory'
+import { makeUpadateCategoryController } from '../factories/categories/update-category-controller-factory'
 
 export default (router: Router): void => {
   router.post('/category', adaptRoute(makeCreateCategoryController()))
+  router.put('/category', adaptRoute(makeUpadateCategoryController()))
 }
