1eda77001d88a92c54c4967dcf3b4312cfee0aee
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-29 00:30:42 -0300

    feat: add router from GetTransactionPeriodCsvController

--- src/main/routes/transaction-routes.test.ts
+++ src/main/routes/transaction-routes.test.ts
@@ -22,7 +22,18 @@ describe('Transaction Router', () => {
         value: 1,
         operation: 'in'
       }).then(async () => {
-        await request(app).get('/transaction?page=0&perpage=2&startdate=2020-01-01&enddate=2022-01-01')
+        await request(app).get('/transaction?page=0&perpage=2&startdate=2020-01-01&enddate=3000-01-01')
+          .expect(200)
+      })
+  })
+
+  test('Should return an list transactions on success', async () => {
+    await request(app).post('/transaction ')
+      .send({
+        value: 1,
+        operation: 'in'
+      }).then(async () => {
+        await request(app).get('/transaction/export?page=0&perpage=2&startdate=2020-01-01&enddate=3000-01-01')
           .expect(200)
       })
   })
