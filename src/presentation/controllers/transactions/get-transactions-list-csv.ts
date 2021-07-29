ce0b0130098ed9da5eaf3a5522e8f5c37b32e243
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-28 22:39:53 -0300

    feat: ensure GetTransactionsListCsvController returns 500 if getTransaction

---
+++ src/presentation/controllers/transactions/get-transactions-list-csv.ts
@@ -0,0 +1,32 @@
+import { GetTransactionsPeriodCsv } from '../../../domain/usecases/transactions/get-transaction-period-csv'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
+import { Controller } from '../../protocols/controller'
+import { HttpRequest, HttpResponse } from '../../protocols/http'
+import { Validation } from '../../protocols/validation'
+
+export class GetTransactionsListCsvController implements Controller {
+  constructor (
+    private readonly validator: Validation,
+    private readonly getTransactions: GetTransactionsPeriodCsv
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const querys = httpRequest.query
+      const isValidOperation = this.validator.validate(querys)
+
+      if (isValidOperation) {
+        return badRequest(isValidOperation)
+      }
+      const page = httpRequest?.query?.page
+      const perpage = httpRequest?.query?.perpage
+      const startDate = httpRequest?.query?.startdate
+      const endDate = httpRequest?.query?.enddate
+
+      const transactions = await this.getTransactions.getTransactions(page, perpage, startDate, endDate)
+      return ok(transactions)
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
