f25a5f8ed694bdb0a03a415f04bdc0371a09e969
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-29 17:43:19 -0300

    test: ensure DbAccout returns an account on success

--- src/data/usecases/account/db-add-account.spec.ts
+++ src/data/usecases/account/db-add-account.spec.ts
@@ -1,19 +1,34 @@
+import { AccountModel } from '../../../domain/models/user'
 import { Encrypter } from '../../protocols/encrypter'
+import { AddAccountRepository } from '../../protocols/users/add-account-repository'
 import { DbAddAccount } from './db-add-account'
 
 describe('DbAddAccount Usercase', () => {
   const makeSut = (): any => {
+    const accountFake = {
+      id: 1,
+      name: 'any_name',
+      user: 'any_user',
+      password: 'any_password'
+    }
     class EncrypterStub implements Encrypter {
       async encrypt (value: string): Promise<string> {
-        return new Promise(resolve => resolve('h ash_passowrd'))
+        return new Promise(resolve => resolve('hash_passowrd'))
+      }
+    }
+    class AddAccountRepositoryStub implements AddAccountRepository {
+      async add (AddAccountModel): Promise<AccountModel> {
+        return new Promise(resolve => resolve(accountFake))
       }
     }
     const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
-
+    const addAccountRepositoryStub = new AddAccountRepositoryStub()
+    const sut = new DbAddAccount(encrypterStub, addAccountRepositoryStub)
     return {
       sut,
-      encrypterStub
+      encrypterStub,
+      accountFake,
+      addAccountRepositoryStub
     }
   }
 
@@ -39,5 +54,17 @@ describe('DbAddAccount Usercase', () => {
     }
     const promise = sut.add(accountdata)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should DbAddAccount return accout on success', async () => {
+    const { sut, accountFake } = makeSut()
+    const categoryData = {
+      name: 'any_name',
+      user: 'any_user',
+      password: 'hash_password'
+    }
+    const account = await sut.add(categoryData)
+    console.log(account)
+    expect(account).toEqual(accountFake)
   })
 })
