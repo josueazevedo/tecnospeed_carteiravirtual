be8fe72805d73e6b420f0b7d5c1581bd15c088ce
Josué Azevedo <josue-azevedo@hotmail.com>
2021-07-29 13:06:23 -0300

    feat: add GetCategoryController router

--- src/main/routes/category-routes.ts
+++ src/main/routes/category-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapter/express-route-adapter'
 import { makeCreateCategoryController } from '../factories/categories/create-category-controller-factory'
+import { makeGetCategoryController } from '../factories/categories/get-categories-controller-factory'
 import { makeUpadateCategoryController } from '../factories/categories/update-category-controller-factory'
 
 export default (router: Router): void => {
   router.post('/category', adaptRoute(makeCreateCategoryController()))
   router.put('/category', adaptRoute(makeUpadateCategoryController()))
+  router.get('/category', adaptRoute(makeGetCategoryController()))
 }
